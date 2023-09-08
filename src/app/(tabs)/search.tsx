a47d72cd9d509c06c80e58951a389e13051b6ec2
Vadim Savin <savinvadim1312@gmail.com>
2023-09-08 16:24:46 +0100

    Search & Favorites screen

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,35 +1,50 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, TextInput, View, Text, StyleSheet } from 'react-native';
+import { tracks } from '../../../assets/data/tracks';
+import TrackListItem from '../../components/TrackListItem';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState } from 'react';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+export default function SearchScreen() {
+  const [search, setSearch] = useState('');
 
-export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+    <SafeAreaView>
+      <View style={styles.header}>
+        {/* Header */}
+        <FontAwesome name="search" size={16} color="gray" />
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="What do you want to listen to?"
+          style={styles.input}
+        />
+        <Text onPress={() => setSearch('')} style={{ color: 'white' }}>
+          Cancel
+        </Text>
+      </View>
+
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+        showsVerticalScrollIndicator={false}
       />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  input: {
+    flex: 1,
+    backgroundColor: '#121314',
+    padding: 8,
+    marginHorizontal: 10,
+    borderRadius: 5,
+    color: 'white',
   },
 });
