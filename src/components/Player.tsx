fc85fc1fc100e88de2c8ca32dbbbc492dbed2b78
Vadim Savin <savinvadim1312@gmail.com>
2023-09-08 17:21:35 +0100

    Play Audie with Expo AV

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,9 +1,63 @@
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { usePlayerContext } from '../providers/PlayerProvider';
+import { useEffect, useState } from 'react';
+import { AVPlaybackStatus, Audio } from 'expo-av';
+import { Sound } from 'expo-av/build/Audio';
 
 const Player = () => {
+  const [sound, setSound] = useState<Sound>();
+  const [isPlaying, setIsPlaying] = useState(false);
   const { track } = usePlayerContext();
+
+  useEffect(() => {
+    playTrack();
+  }, [track]);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const playTrack = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    if (!track?.preview_url) {
+      return;
+    }
+    const { sound: newSound } = await Audio.Sound.createAsync({
+      uri: track.preview_url,
+    });
+
+    setSound(newSound);
+    newSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    await newSound.playAsync();
+  };
+
+  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) {
+      return;
+    }
+
+    setIsPlaying(status.isPlaying);
+  };
+
+  const onPlayPause = async () => {
+    if (!sound) {
+      return;
+    }
+    if (isPlaying) {
+      await sound.pauseAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
 
   if (!track) {
     return null;
@@ -28,8 +82,9 @@ const Player = () => {
           style={{ marginHorizontal: 10 }}
         />
         <Ionicons
+          onPress={onPlayPause}
           disabled={!track?.preview_url}
-          name={'play'}
+          name={isPlaying ? 'pause' : 'play'}
           size={22}
           color={track?.preview_url ? 'white' : 'gray'}
         />
