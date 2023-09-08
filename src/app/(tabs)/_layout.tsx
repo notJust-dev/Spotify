9dba266d274162332a5129af3b374a1112aab455
Vadim Savin <savinvadim1312@gmail.com>
2023-09-08 16:42:51 +0100

    Music Player UI

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import { Pressable, useColorScheme, Text, View } from 'react-native';
+import { BottomTabBar } from '@react-navigation/bottom-tabs';
 
 import Colors from '../../constants/Colors';
+import Player from '../../components/Player';
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
@@ -22,6 +24,12 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
       }}
+      tabBar={(props) => (
+        <View>
+          <Player />
+          <BottomTabBar {...props} />
+        </View>
+      )}
     >
       <Tabs.Screen
         name="index"
---
+++ src/components/Player.tsx
@@ -0,0 +1,72 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { tracks } from '../../assets/data/tracks';
+const track = tracks[0];
+
+const Player = () => {
+  if (!track) {
+    return null;
+  }
+
+  const image = track.album.images?.[0];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.player}>
+        {image && <Image source={{ uri: image.url }} style={styles.image} />}
+
+        <View style={{ flex: 1 }}>
+          <Text style={styles.title}>{track.name}</Text>
+          <Text style={styles.subtitle}>{track.artists[0]?.name}</Text>
+        </View>
+
+        <Ionicons
+          name={'heart-outline'}
+          size={20}
+          color={'white'}
+          style={{ marginHorizontal: 10 }}
+        />
+        <Ionicons
+          disabled={!track?.preview_url}
+          name={'play'}
+          size={22}
+          color={track?.preview_url ? 'white' : 'gray'}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: -75,
+    width: '100%',
+    height: 75,
+    padding: 10,
+  },
+  player: {
+    backgroundColor: '#286660',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 5,
+    padding: 3,
+    paddingRight: 15,
+  },
+  title: {
+    color: 'white',
+  },
+  subtitle: {
+    color: 'lightgray',
+    fontSize: 12,
+  },
+  image: {
+    height: '100%',
+    aspectRatio: 1,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+});
+
+export default Player;
