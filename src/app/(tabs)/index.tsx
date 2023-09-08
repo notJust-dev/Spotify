ec3603fa069d84d0b63e98ec023b144b501c4eb1
Vadim Savin <savinvadim1312@gmail.com>
2023-09-08 18:51:39 +0100

    Recommendation Query

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,49 @@
-import { FlatList } from 'react-native';
-import { tracks } from '../../../assets/data/tracks';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import TrackListItem from '../../components/TrackListItem';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            url
+            width
+            height
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function HomeScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: 'drum-and-bass,house' },
+  });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return (
+      <Text style={{ color: 'white' }}>Failed to fetch recommendations</Text>
+    );
+  }
+
+  const tracks = data?.recommendations?.tracks || [];
+
   return (
     <FlatList
       data={tracks}
